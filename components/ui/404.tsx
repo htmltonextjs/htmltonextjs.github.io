617273febb581da946f30e446113c3137045d8ed
faikit <huijie.wang@hotmail.com>
2025-05-22 23:10:48 +0800

    .

--- components/ui/404.tsx
+++ components/ui/404.tsx
@@ -69,9 +69,51 @@ export default function NotFound404() {
     if (pathname.toLowerCase().startsWith('/elm')) {
         redirect('https://opendrivelab.github.io/elm.github.io/')
     }
+    // challenge
+    if (
+        pathname.toLowerCase().startsWith('/AD23Challenge') || 
+        pathname.toLowerCase() == '/challenge2023/') {
+        redirect('/legacy/challenge2023/index.html')
+    }
+    if (
+        pathname.toLowerCase().startsWith('/cvpr2024/challenge') || 
+        pathname.toLowerCase() == '/challenge2024/') {
+        redirect('/legacy/challenge2024/index.html')
+    }
+    if (
+        pathname.toLowerCase().startsWith('/challenge2025/temp') || 
+        pathname.toLowerCase() == '/challenge2025/'
+    ) {
+        redirect('/legacy/challenge2025/index.html')
+    }
+    // workshop / tutorial
+    if (
+        pathname.toLowerCase().startsWith('/event/iclr23_ADworkshop') || 
+        pathname.toLowerCase().startsWith('/sr4ad/iclr23') || 
+        pathname.toLowerCase() == '/iclr2023/workshop/') {
+        redirect('/legacy/iclr2023/workshop/index.html')
+    }
+    if (
+        pathname.toLowerCase().startsWith('/event/cvpr23_ADworkshop') ||
+        pathname.toLowerCase() == '/e2ead/cvpr23' ||
+        pathname.toLowerCase() == '/e2ead/cvpr23/' || 
+        pathname.toLowerCase() == '/cvpr2023/workshop/') {
+        redirect('/legacy/cvpr2023/workshop/index.html')
+    }
+    if (pathname.toLowerCase().startsWith('/valse2024')) {
+        redirect('/legacy/valse2024/workshop/index.html')
+    }
     // misc.
     if (pathname.toLowerCase().startsWith('/lihongyang')) {
         redirect('https://lihongyang.info/')
+    }
+    if (
+        pathname.toLowerCase().startsWith('/e2ead/cvpr23_CN') || 
+        pathname.toLowerCase() == '/event2023/') {
+        redirect('/legacy/event2023/index.html')
+    }
+        if (pathname.toLowerCase() == '/event2024/') {
+        redirect('https://starleague.ai/event2024/')
     }
 
     return (
